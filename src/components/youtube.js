a58dcb2300f0496a3fa1f3f3ff73da555c3ddcfd
Byron Lee <byronn.lee@gmail.com>
2022-01-02 20:10:11 +0000

    created the basic app with youtube reqest

---
+++ src/components/youtube.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    type: 'video',
+    maxResults: 5,
+    key: process.env.REACT_APP_YOU_TUBE_API_KEY,
+
+  }
+});
