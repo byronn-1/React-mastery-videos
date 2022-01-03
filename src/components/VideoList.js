ccac2f7dd519ad73de8cc84b1e8369a6e6e7d16d
Byron Lee <byronn.lee@gmail.com>
2022-01-03 08:57:24 +0000

    completed project

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import VideoItem from './VideoItem';
 
-const VideoList = (props) => {
-
+const VideoList = ({ videos, onVideoSelect }) => {
+  const renderedList = videos.map((video) => {
+    return <VideoItem
+      onVideoSelect={onVideoSelect}
+      video={video}
+      key={video.id.videoId}
+    />
+  })
 
   return (
-    <div>{props.videos.length}</div>
+    <div>{renderedList}</div>
   )
 }
 
 export default VideoList;
+
